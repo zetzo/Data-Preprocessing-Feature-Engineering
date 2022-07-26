cec79e2421969c44964e1644c3ea9664c98b8b58
zetzo <39604818+zetzo@users.noreply.github.com>
2022-07-26 23:20:02 +0200

    Create extract_miles.py

---
+++ extract_miles.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+run_data = pd.DataFrame({"runner": [34, 35, 36, 37, 38], 
+            "max": ["15 miles", "20 miles", "17 miles", 
+            "30 miles", "10 miles"]})
+
+print(run_data)
